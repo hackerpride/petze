471f58cb093624ef746b87d2081a490fffa7c5ea
Maximilian Goisser <max@massagio.de>
2016-12-03 21:49:35 +0100

    feat(android_beacon): get beacon working on android via kontaktio package

--- src/native/index.js
+++ src/native/index.js
@@ -3,8 +3,12 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  DeviceEventEmitter,
+  PermissionsAndroid,
 } from 'react-native';
+
+import KontaktBeacons from 'react-native-kontaktio';
 
 export default function index() {
 
@@ -28,6 +32,88 @@ export default function index() {
   });
 
   class Root extends Component {
+    componentDidMount() {
+      KontaktBeacons.initKontaktSDKWithApiKey('MY_KONTAKTIO_API_KEY');
+      DeviceEventEmitter.addListener(
+        'beaconsDidRange',
+        (data) => {
+          console.log("HERE1");
+          console.log(data);
+          // this.setState({
+          //   beacons: data.beacons.filter(beacon => beacon.rssi < 0),
+          // });
+          // data.beacons.map(beacon => console.log('minor', beacon.minor));
+        }
+      );
+      DeviceEventEmitter.addListener(
+        'scanInitStatus',
+        (data) => {
+          console.log("HERE2");
+          // this.setState({ scanInitStatus: data.status });
+        }
+      );
+      DeviceEventEmitter.addListener(
+        'scanStatus',
+        (data) => {
+          console.log("HERE3");
+          // this.setState({ scanStatus: data.scanStatus });
+        }
+      );
+
+      const region = {
+        identifier: 'MY_BEACON_REGION',
+        uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e',
+        major: 44703,
+        minor: KontaktBeacons.ANY_MINOR,
+      };
+      const scanContext = {
+        iBeaconDevicesUpdateCallbackInterval: 100,
+        eddystoneDevicesUpdateCallbackInterval: 1000,
+        iBeaconDistanceSort: KontaktBeacons.SORT_DESC,
+        eddystoneDistanceSort: KontaktBeacons.SORT_ASC,
+      };
+      KontaktBeacons.startRangingBeaconsInRegion(region, scanContext);
+    }
+
+    async requestPermissions() {
+      try {
+        const granted = await PermissionsAndroid.requestPermission(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+          {
+            'title': 'Cool Photo App Camera Permission',
+            'message': 'Cool Photo App needs access to your camera ' +
+                       'so you can take awesome pictures.'
+          }
+        )
+        if (granted) {
+          console.log("ACCESS_COARSE_LOCATION permission granted")
+        } else {
+          console.log("ACCESS_COARSE_LOCATION permission denied")
+        }
+      } catch (err) {
+        console.log("ERROR");
+        console.warn(err)
+      }
+      try {
+        const granted = await PermissionsAndroid.requestPermission(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            'title': 'Cool Photo App Camera Permission',
+            'message': 'Cool Photo App needs access to your camera ' +
+                       'so you can take awesome pictures.'
+          }
+        )
+        if (granted) {
+          console.log("ACCESS_FINE_LOCATION permission granted")
+        } else {
+          console.log("ACCESS_FINE_LOCATION permission denied")
+        }
+      } catch (err) {
+        console.log("ERROR");
+        console.warn(err)
+      }
+    }
+
     render() {
       return (
         <View style={styles.container}>
